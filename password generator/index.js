e0537eb13916797efc7ffecbd3620227321a931e
BuildTools <unconfigured@null.spigotmc.org>
2022-05-20 07:40:11 -0700

    js is done

--- password generator/index.js
+++ password generator/index.js
@@ -1,3 +1,24 @@
+var possibleChars = []
+var passLength = 10
+
+for (let i = 33; i < 126; i++) {
+    possibleChars.push(String.fromCharCode(i))
+}
+
+var options = document.querySelectorAll("input.passOption")
+
+function renderPass() {
+    options.forEach(option => {
+        option.style.backgroundImage = "none";
+        option.value = genPass()
+    })
+}
+
 function genPass() {
-    console.log(1)
+    var password = ''
+    for (var i = 0; i < passLength; i++) {
+        password += possibleChars[Math.floor(Math.random() * possibleChars.length)]
+    }
+    return password
+
 }
